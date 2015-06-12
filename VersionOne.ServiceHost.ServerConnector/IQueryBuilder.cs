b2d6b5ac8b1422664f27a953810752e1107498f1
Santiago Aceñolaza <chanacenolaza@gmail.com>
2015-06-12 14:51:47 -0300

    Switched to 15.0 version of the APIClient SDK

--- VersionOne.ServiceHost.ServerConnector/IQueryBuilder.cs
+++ VersionOne.ServiceHost.ServerConnector/IQueryBuilder.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Generic;
+﻿/*(c) Copyright 2012, VersionOne, Inc. All rights reserved. (c)*/
+using System.Collections.Generic;
 using VersionOne.SDK.APIClient;
 using VersionOne.ServerConnector.Entities;
 using VersionOne.ServerConnector.Filters;
 
-namespace VersionOne.ServerConnector {
+namespace VersionOne.ServerConnector
+{
     // TODO refactor APIClient types
-    public interface IQueryBuilder {
+    public interface IQueryBuilder
+    {
         IDictionary<string, PropertyValues> ListPropertyValues { get; }
         IEntityFieldTypeResolver TypeResolver { get; }
-        IEnumerable<AttributeInfo> AttributesToQuery { get; } 
+        IEnumerable<AttributeInfo> AttributesToQuery { get; }
 
-        void Setup(IServices services, IMetaModel metaModel, ILocalizer localizer);
+        void Setup(IServices services);
         void AddProperty(string attr, string prefix, bool isList);
         void AddListProperty(string fieldName, string typeToken);
         void AddOptionalProperty(string attr, string prefix);
